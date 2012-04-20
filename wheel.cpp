aba90b4f6e9d8a02eeefdb6e291514ba266a0d96
Serge Matveenko <s@matveenko.ru>
2012-04-21 02:15:37 +0400

    Wheel loop counter.

--- wheel.cpp
+++ wheel.cpp
@@ -1,9 +1,10 @@
 #include "std_lib_facilities.h"
 
 int main() {
+	int i=0;
 	cout << "The wheel is spinning\n";
 	while (1) {
-		cout << "\tand spinning\n";
+		cout << "\tand spinning (" << ++i << ")\n";
 	}
 	return 0;
 }
