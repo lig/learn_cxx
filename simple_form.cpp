677f543a69e623d37bfd62dfbf0c83ae3a36ea01
Serge Matveenko <s@matveenko.ru>
2012-05-02 00:19:10 +0400

    Add simple_form.cpp.

---
+++ simple_form.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+
+int main() {
+	cout << "Please type your name than press enter:\n";
+	string first_name;
+	cin >> first_name;
+	cout << "Hello, " << first_name << "!\n";
+}
