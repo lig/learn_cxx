7a117fb0b3e27ce773b9e226d6d16be8e69d3930
Serge Matveenko <s@matveenko.ru>
2012-05-02 00:26:49 +0400

    Add friend name to message form.

--- simple_form.cpp
+++ simple_form.cpp
@@ -4,5 +4,11 @@ int main() {
 	cout << "Please type recipient name than press enter:\n";
 	string first_name;
 	cin >> first_name;
+	cout << "Please type friend name than press enter:\n";
+	string friend_name;
+	cin >> friend_name;
 	cout << "Dear " << first_name << ",\n";
+	cout << "\n";
+	cout << "Whazup?\n";
+	cout << "Have you seen " << friend_name << " recently?";
 }
