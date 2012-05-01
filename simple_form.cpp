e1d30069e1c628216e048d31d59747ab2da9f557
Serge Matveenko <s@matveenko.ru>
2012-05-02 00:21:37 +0400

    Edit simple form to be mail message form.

--- simple_form.cpp
+++ simple_form.cpp
@@ -1,8 +1,8 @@
 #include "std_lib_facilities.h"
 
 int main() {
-	cout << "Please type your name than press enter:\n";
+	cout << "Please type recipient name than press enter:\n";
 	string first_name;
 	cin >> first_name;
-	cout << "Hello, " << first_name << "!\n";
+	cout << "Dear " << first_name << ",\n";
 }
