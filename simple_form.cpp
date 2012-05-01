886748eea8a452369fa807a5d8b1b7996395974d
Serge Matveenko <s@matveenko.ru>
2012-05-02 00:34:59 +0400

    Add friend sex to message form.

--- simple_form.cpp
+++ simple_form.cpp
@@ -7,8 +7,17 @@ int main() {
 	cout << "Please type friend name than press enter:\n";
 	string friend_name;
 	cin >> friend_name;
+	cout << "Please specify " << friend_name << "'s sex (type 'm' if mail or 'f' if female):\n";
+	char friend_sex=0;
+	cin >> friend_sex;
 	cout << "Dear " << first_name << ",\n";
 	cout << "\n";
 	cout << "Whazup?\n";
-	cout << "Have you seen " << friend_name << " recently?";
+	cout << "Have you seen " << friend_name << " recently?\n";
+	if (friend_sex == 'm') {
+		cout << "If you see " << friend_name << ", tell him to call me.\n";
+	}
+	if (friend_sex == 'f') {
+		cout << "If you see " << friend_name << ", tell her to call me.\n";
+	}
 }
