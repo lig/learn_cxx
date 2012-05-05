09f4d722ed70022141cd221f5088837352b74abd
Serge Matveenko <s@matveenko.ru>
2012-05-06 00:35:16 +0400

    Add C to K converter. Low bound check inside conversion function.

---
+++ chptr_I5_wrkt_4.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+double c2k(double c) {
+	if (c < -273.15) {
+		throw runtime_error("Incorrect value");
+	}
+	double k = c + 273.15;
+	return k;
+}
+
+int main() {
+	double c = 0;
+	cin >> c;
+	double k = c2k(c);
+	cout << k << endl;
+}
