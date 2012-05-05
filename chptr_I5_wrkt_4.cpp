63af985222d4d6e5cb70cf46b69a43c181f1855c
Serge Matveenko <s@matveenko.ru>
2012-05-06 00:35:29 +0400

    Depend low temp bound check on 0°K.

--- chptr_I5_wrkt_4.cpp
+++ chptr_I5_wrkt_4.cpp
@@ -1,8 +1,16 @@
 #include "std_lib_facilities.h"
 
+double k2c(double k) {
+	if (k < 0) {
+		throw runtime_error("Incorrect K value");
+	}
+	double c = k - 273.15;
+	return c;
+}
+
 double c2k(double c) {
-	if (c < -273.15) {
-		throw runtime_error("Incorrect value");
+	if (c < k2c(0)) {
+		throw runtime_error("Incorrect C value");
 	}
 	double k = c + 273.15;
 	return k;
