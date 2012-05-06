e426c89dd4c6dcaaccc966c80fd69d3c5d9a9b13
Serge Matveenko <s@matveenko.ru>
2012-05-06 10:22:06 +0400

    Add sum of the N first entered numbers.

---
+++ chptr_I5_wrkt_8.cpp
@@ -0,0 +1,28 @@
+#include "std_lib_facilities.h"
+
+int main() {
+	vector<int> numbers;
+	int number;
+
+	cout << "Please enter several integer numbers (end with '|' symbol):" << endl;
+	while (cin >> number) {
+		numbers.push_back(number);
+	}
+	cin.clear();
+	cin.ignore();
+
+	int count;
+	cout << "Please enter how much numbers you want to sum up (not more than " << numbers.size() << "):" << endl;
+	if (!(cin >> count)) {
+		throw runtime_error("Incorrect number");
+	}
+	if (count > numbers.size()) {
+		throw runtime_error("Number too big");
+	}
+
+	int sum = 0;
+	for (int i = 0; i < count; ++i) {
+		sum += numbers[i];
+	}
+	cout << "The sum of the first " << count << " numbers is " << sum << endl;
+}
