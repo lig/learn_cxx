3961d80fc8d5eeb0cea66bd5452f5db11333f98f
Serge Matveenko <s@matveenko.ru>
2012-12-06 15:35:18 +0400

    Exit via "x" instead of "q".

--- chptr_I6_task.cpp
+++ chptr_I6_task.cpp
@@ -76,7 +76,7 @@ Token Token_stream::get()
 
     switch (ch) {
     case ';':    // for "print"
-    case 'q':    // for "quit"
+    case 'x':    // for "quit"
     case '(': case ')': case '+': case '-': case '*': case '/':
         return Token(ch);        // let each character represent itself
     case '.':
@@ -185,7 +185,7 @@ try
     while (cin) {
         Token t = ts.get();
 
-        if (t.kind == 'q') break; // 'q' for quit
+        if (t.kind == 'x') break; // 'x' for quit
         if (t.kind == ';') {        // ';' for "print now"
         	cout << "=" << val << '\n';
         }
